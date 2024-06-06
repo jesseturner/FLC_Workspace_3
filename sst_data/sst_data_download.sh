09d5daec36c80ba95cd5db7134b505bbbde39939
Jesse Turner <jturner@gennai.local>
2024-06-06 09:42:05 -0600

    Adding SST data download

---
+++ sst_data/sst_data_download.sh
@@ -0,0 +1,11 @@
+for date in 20240602
+
+do 
+	#--- SST is only tracked once per day, so no time variable
+    sst=https://www.ncei.noaa.gov/thredds/fileServer/OisstBase/NetCDF/V2.1/AVHRR/${date:0:6}/oisst-avhrr-v02r01."$date".nc
+    sst_backup=https://www.ncei.noaa.gov/thredds/fileServer/OisstBase/NetCDF/V2.1/AVHRR/${date:0:6}/oisst-avhrr-v02r01."$date"_preliminary.nc
+    
+    wget --no-check-certificate -O sst_data/sst_$date $sst || wget -O sst_data/sst_$date $sst_backup
+    echo "SST collected for "$date
+
+done
