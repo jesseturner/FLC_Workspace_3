d0a52995dcbfb0ae433dad8f4dfbea0a92ad714b
Jesse Turner <jesseturner72@gmail.com>
2024-06-12 10:16:19 -0600

    Plotting the satellite BTD

--- model_data/model_data_download.sh
+++ model_data/model_data_download.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-for date in 20240606
+for date in 20230907
 
 do 
 	#--- must be 00z, 06z, 12z, 18z
--- sst_data/sst_data_download.sh
+++ sst_data/sst_data_download.sh
@@ -1,4 +1,4 @@
-for date in 20240606
+for date in 20230907
 
 do 
 	#--- SST is only tracked once per day, so no time variable
