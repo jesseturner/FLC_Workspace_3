6cd6758248346d09170f77b47712f37853b09aae
Jesse Turner <jesseturner72@gmail.com>
2024-09-12 10:37:54 -0600

    Running imagery

--- model_data/model_data_download.sh
+++ model_data/model_data_download.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
-for date in 20240629
+for date in 20240620
 
 do 
 	#--- must be 00z, 06z, 12z, 18z
-	dtime=06z
+	dtime=00z
 
 	#--- data can be browsed at: https://noaa-gfs-bdp-pds.s3.amazonaws.com/index.html
 	wget -O model_data/gfs_$date https://noaa-gfs-bdp-pds.s3.amazonaws.com/gfs."$date"/${dtime:0:2}/atmos/gfs.t"$dtime".pgrb2.0p25.f000
